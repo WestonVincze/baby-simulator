40bbc4e757da4fed67a87c516e36ba6641805e10
Weston Vincze <westonvincze@gmail.com>
2024-09-22 23:55:08 -0400

    📝Add brainstorming notes

---
+++ src/stores/Brain.ts
@@ -0,0 +1,17 @@
+/**
+ * considerations
+ * * base
+ * * * (TODO LATER)
+ * * contextual
+ * * * each contextual consideration is the desire to play with a specific toy on the play mat
+ * 
+ * 
+ * process:
+ * * maintain a list of considerations based on the available toys
+ * * * when a new toy is added, add a consideration to play with it
+ * * * * create necessary appraisals for the consideration
+ * * * when a toy is removed from the field, remove it
+ * * evaluate every consideration
+ * 
+ * 
+ */
