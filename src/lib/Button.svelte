dbf15a39cb8e57b3efc958790e0e46c81c61c9c8
Weston Vincze <westonvincze@gmail.com>
2024-10-16 23:55:39 -0400

    ✨ Button placeholder

---
+++ src/lib/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let label: string;
+  let href: string;
+
+</script>
+
+<a {href}>{label}</a>
+
+<style>
+  a {
+    background-color: #704264;
+  }
+
+  a:hover {
+    background-color: #70426455;
+  }
+</style>
