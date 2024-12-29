6cf6f9522a37e9496bbc815f025c5b64cd4e5f86
Weston Vincze <westonvincze@gmail.com>
2024-12-28 23:55:38 -0500

    🚧 Scaffold for appeal consideration

---
+++ src/ai/Considerations/AppealConsideration.ts
@@ -0,0 +1,29 @@
+/**
+ * Calculates the overall appeal of a Toy
+ */
+
+/** CONTEXT
+ * baby data
+ * * attribute aversions
+ * * attribute preferences
+ * toy data
+ * * condensed attribute data
+ */
+
+const AppealConsideration = (aversions: any, preferences: any, attributes: any) => {
+  /** calculate Aversion */
+  // create empty array 
+  // iterate through aversions
+  // calculate the NBA for matching attributes
+
+  /** calculate Preferences */
+  // create empty array 
+  // iterate through preferences
+  // calculate the NBA for matching attributes
+
+  /** calculate total */
+  // combine aversions and preferences, merging any duplicate attributes
+  // return the average of each value in the array
+  // final value should be 0-1 where 0 is no appeal and 1 is maximum appeal
+  return 0;
+}
